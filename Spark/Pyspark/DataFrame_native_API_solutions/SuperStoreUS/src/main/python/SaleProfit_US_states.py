7c0f9b80027b924a30166d519ac8fe4f946abd55
Manohar <manosoftware1@gmail.com>
2019-02-02 17:50:27 +0530

    changes on 02-Feb-2019 for pyspark code
    changes on 02-Feb-2019 for pyspark code

--- Spark/Pyspark/DataFrame_native_API_solutions/SuperStoreUS/src/main/python/SaleProfit_US_states.py
+++ Spark/Pyspark/DataFrame_native_API_solutions/SuperStoreUS/src/main/python/SaleProfit_US_states.py
@@ -2,59 +2,54 @@ import sys
 import configparser as config
 import datetime
 
-try:
-    from pyspark.sql import SparkSession
-    from pyspark.sql.types import DoubleType
-    from pyspark.sql.types import IntegerType
-    from pyspark.sql.functions import *
+from pyspark.sql import SparkSession
+from pyspark.sql.types import DoubleType
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import *
 
-    props = config.RawConfigParser()
-    props.read("src/main/configs/app.properties")
-    exMode = sys.argv[1]
+props = config.RawConfigParser()
+props.read("src/main/configs/app.properties")
+exMode = sys.argv[1]
 
-    spark = SparkSession.\
-        builder.\
-        master(props.get(exMode, 'deployMode')).\
-        appName("Sale Profit Per State in US").\
-        getOrCreate()
+spark = SparkSession.\
+    builder.\
+    master(props.get(exMode, 'deployMode')).\
+    appName("Sale Profit Per State in US").\
+    getOrCreate()
 
-    sc = spark.sparkContext
+sc = spark.sparkContext
 
-    # Handling with file system
-    dataInPath = props.get(exMode, 'data.in.path')
-    dataOutPath = props.get(exMode, 'data.out.path')
+# Handling with file system
+dataInPath = props.get(exMode, 'data.in.path')
+dataOutPath = props.get(exMode, 'data.out.path')
 
-    # HDFS API's
-    Path = sc._gateway.jvm.org.apache.hadoop.fs.Path
-    FileSytem = sc._gateway.jvm.org.apache.hadoop.fs.FileSystem
-    Configuration = sc._gateway.jvm.org.apache.hadoop.conf.Configuration
+# HDFS API's
+Path = sc._gateway.jvm.org.apache.hadoop.fs.Path
+FileSytem = sc._gateway.jvm.org.apache.hadoop.fs.FileSystem
+Configuration = sc._gateway.jvm.org.apache.hadoop.conf.Configuration
 
-    fs = FileSytem.get(Configuration())
+fs = FileSytem.get(Configuration())
 
-    if (fs.exists(Path(dataInPath)) == True):
-        if (fs.exists(Path(dataOutPath))):
-            dataOutPath = dataOutPath + "_" + str(datetime.datetime.now().strftime("%y_%m_%d_%H_%M"))
-            print("New Path = ", dataOutPath)
+if (fs.exists(Path(dataInPath)) == True):
+    if (fs.exists(Path(dataOutPath))):
+        dataOutPath = dataOutPath + "_" + str(datetime.datetime.now().strftime("%y_%m_%d_%H_%M"))
+        print("New Path = ", dataOutPath)
 
-        dataDF = spark.read.csv(dataInPath,
-                            header= True, inferSchema= True)
+    dataDF = spark.read.csv(dataInPath,
+                        header= True, inferSchema= True)
 
-        #Type casting - since the inferScheame option doesn't properly infer the data type
-        dataDF = dataDF.withColumn("Sales", dataDF.Sales.cast(DoubleType())).\
-            withColumn("Quantity", dataDF.Quantity.cast(IntegerType())).\
-            withColumn("Discount", dataDF.Discount.cast(DoubleType()))
+    #Type casting - since the inferScheame option doesn't properly infer the data type
+    dataDF = dataDF.withColumn("Sales", dataDF.Sales.cast(DoubleType())).\
+        withColumn("Quantity", dataDF.Quantity.cast(IntegerType())).\
+        withColumn("Discount", dataDF.Discount.cast(DoubleType()))
 
-        dataAggDF = dataDF.select(dataDF.Country, dataDF.State, dataDF.Profit).\
-            groupBy("Country", "State").\
-            agg(round(sum(dataDF.Profit), 2).alias("Total_profit"))
+    dataAggDF = dataDF.select(dataDF.Country, dataDF.State, dataDF.Profit).\
+        groupBy("Country", "State").\
+        agg(round(sum(dataDF.Profit), 2).alias("Total_profit"))
 
-        dataAggDF.orderBy(dataAggDF.Total_profit.desc()).\
-            coalesce(2).\
-            write.\
-            csv(dataOutPath, mode= "overwrite")
-    else:
-        print("Input Path is not exist" + dataInPath)
-
-except ImportError as ie:
-    print("Import Error with Spark modules", ie)
-    sys.exit(1)
+    dataAggDF.orderBy(dataAggDF.Total_profit.desc()).\
+        coalesce(2).\
+        write.\
+        csv(dataOutPath, mode= "overwrite")
+else:
+    print("Input Path is not exist" + dataInPath)
